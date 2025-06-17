5b4223f155be59ef8705ccffd115285c90ac3e8e
pkumv1 <45692629+pkumv1@users.noreply.github.com>
2025-06-17 13:02:53 +0530

    feat: add App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TfLDashboard from './TfLDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TfLDashboard />
+    </div>
+  );
+}
+
+export default App;
